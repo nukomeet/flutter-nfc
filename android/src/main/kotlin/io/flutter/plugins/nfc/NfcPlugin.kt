9dbadbfe3b94056f3ab872882c8de1e7b9fdfe61
Chaitanya Munukutla <chaitanya.m61292@gmail.com>
2018-03-14 13:32:12 +0530

    Added NFC android bridge

--- android/src/main/kotlin/io/flutter/plugins/nfc/NfcPlugin.kt
+++ android/src/main/kotlin/io/flutter/plugins/nfc/NfcPlugin.kt
@@ -6,20 +6,149 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class NfcPlugin(): MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar): Unit {
-      val channel = MethodChannel(registrar.messenger(), "nfc")
-      channel.setMethodCallHandler(NfcPlugin())
+class NfcPlugin() : MethodCallHandler {
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar): Unit {
+            val channel = MethodChannel(registrar.messenger(), "nfc")
+            channel.setMethodCallHandler(NfcPlugin())
+        }
     }
-  }
 
-  override fun onMethodCall(call: MethodCall, result: Result): Unit {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onMethodCall(call: MethodCall, result: Result): Unit {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else if (call.method.equals("read") {
+            readNFC()
+        } else {
+            result.notImplemented()
+        }
     }
-  }
+
+    private fun readNFC() {
+        nfcAdapter.let {
+            if (nfcAdapter?.isEnabled!!) {
+                print("NFC not enable")
+                return
+            }
+
+            handleIntent(intent)
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        val action = intent.action
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == action) {
+
+            val type = intent.type
+            if (MIME_TEXT_PLAIN == type) {
+
+                val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
+                NdefReaderTask().execute(tag)
+
+            } else {
+                Log.d("Bug", "Wrong mime type: " + type)
+            }
+        } else if (NfcAdapter.ACTION_TECH_DISCOVERED == action) {
+
+            // In case we would still use the Tech Discovered Intent
+            val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
+            val techList = tag.techList
+            val searchedTech = Ndef::class.java.name
+
+            for (tech in techList) {
+                if (searchedTech == tech) {
+                    NdefReaderTask().execute(tag)
+                    break
+                }
+            }
+        }
+    }
+
+    fun setupForegroundDispatch(activity: Activity, adapter: NfcAdapter) {
+        val intent = Intent(activity.applicationContext, activity.javaClass)
+        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+
+        val pendingIntent = PendingIntent.getActivity(activity.applicationContext, 0, intent, 0)
+
+        val filters = arrayOfNulls<IntentFilter>(1)
+        val techList = arrayOf<Array<String>>()
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = IntentFilter()
+        filters[0]?.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED)
+        filters[0]?.addCategory(Intent.CATEGORY_DEFAULT)
+        try {
+            filters[0]?.addDataType(MIME_TEXT_PLAIN)
+        } catch (e: MalformedMimeTypeException) {
+            throw RuntimeException("Check your mime type.")
+        }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList)
+    }
+
+    fun stopForegroundDispatch(activity: Activity, adapter: NfcAdapter) {
+        adapter.disableForegroundDispatch(activity)
+    }
+
+    private inner class NdefReaderTask : AsyncTask<Tag, Void, String>() {
+
+        override fun doInBackground(vararg params: Tag): String? {
+            val tag = params[0]
+
+            val ndef = Ndef.get(tag) ?: // NDEF is not supported by this Tag.
+                    return null
+
+            val ndefMessage = ndef.cachedNdefMessage
+
+            val records = ndefMessage.records
+            for (ndefRecord in records) {
+                if (ndefRecord.tnf == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.type, NdefRecord.RTD_TEXT)) {
+                    try {
+                        return readText(ndefRecord)
+                    } catch (e: UnsupportedEncodingException) {
+                        Log.e("Bug", "Unsupported Encoding", e)
+                    }
+
+                }
+            }
+
+            return null
+        }
+
+        @Throws(UnsupportedEncodingException::class)
+        private fun readText(record: NdefRecord): String {
+            /*
+         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
+         *
+         * http://www.nfc-forum.org/specs/
+         *
+         * bit_7 defines encoding
+         * bit_6 reserved for future use, must be 0
+         * bit_5..0 length of IANA language code
+         */
+
+            val payload = record.payload
+
+            // Get the Text Encoding
+            val textEncoding = "UTF-8"
+
+            // Get the Language Code
+            val languageCodeLength = payload[0] and 51
+
+            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+            // e.g. "en"
+
+            // Get the Text
+            return String(payload, languageCodeLength + 1, payload.size - languageCodeLength - 1, Charset.defaultCharset())
+        }
+
+        override fun onPostExecute(result: String?) {
+            if (result != null) {
+                print("Read content: " + result)
+            }
+        }
+    }
+
+
 }
--- lib/nfc.dart
+++ lib/nfc.dart
@@ -3,9 +3,10 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Nfc {
-  static const MethodChannel _channel =
-      const MethodChannel('nfc');
+  static const MethodChannel _channel = const MethodChannel('nfc');
 
   static Future<String> get platformVersion =>
       _channel.invokeMethod('getPlatformVersion');
+
+  static Future<String> get readTag => _channel.invokeMethod('read');
 }
